c5df0af9625d89922ea9f251d769f399b64ba85f
TunYi Chang <tunyic@uci.edu>
2021-05-03 13:49:40 -0700

    db works

---
+++ app/src/main/java/com/example/bankapp/DBHelper.java
@@ -0,0 +1,40 @@
+package com.example.bankapp;
+
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "test.db";
+
+    public DBHelper(Context context){
+        super(context,DATABASE_NAME,null,1);
+        Log.d("asd","constructor");
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.d("asd","created");
+        db.execSQL("CREATE TABLE test (ID INTEGER PRIMARY KEY AUTOINCREMENT)");
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("ID", 30);
+        db.insert("test",null, contentValues);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {}
+
+
+    public Cursor getAllData() {
+        Log.d("asd","getdata");
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from test",null);
+        return res;
+    }
+
+}
