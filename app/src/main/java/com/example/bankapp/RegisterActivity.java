5365c807dcff6ba3aec4435f734da0b411ab2bf8
TunYi Chang <tunyic@uci.edu>
2021-06-08 12:40:26 -0700

    fix register activity

--- app/src/main/java/com/example/bankapp/RegisterActivity.java
+++ app/src/main/java/com/example/bankapp/RegisterActivity.java
@@ -33,27 +33,32 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 TextView warning = (TextView) findViewById(R.id.hint);
-                Cursor res = mydb.getDataByUsername(username.getText().toString());
 
-                if (username.getText().toString().isEmpty() || password.getText().toString().isEmpty() ||
-                        balance.getText().toString().isEmpty() || email.getText().toString().isEmpty()) {
-                    warning.setText("Please fill in all the information");
-                } else if (res.getCount() == 0) {
-                    if (Double.parseDouble(balance.getText().toString()) < 0) {
-                        warning.setText("Please fill in a valid amount.");
-                    }
-                    if(!isValidEmail(email.getText().toString())){    //check if provided email is valid
-                        warning.setText("Invalid email");
-                    }else {
-                        warning.setText("Success!");
-                        boolean ins = mydb.insertUser(username.getText().toString(), password.getText().toString(), Double.parseDouble(balance.getText().toString()), email.getText().toString());
+                if(!username.getText().toString().matches("[0-9a-zA-Z_]+") || !password.getText().toString().matches("[0-9a-zA-Z_]+")){
+                    warning.setText("Invalid username or password");
+                }else {
+                    Cursor res = mydb.getDataByUsername(username.getText().toString());
 
-                        Intent intent = new Intent(getApplicationContext(), OverviewActivity.class);
-                        intent.putExtra("username", username.getText().toString());
-                        startActivity(intent);
+                    if (username.getText().toString().isEmpty() || password.getText().toString().isEmpty() ||
+                            balance.getText().toString().isEmpty() || email.getText().toString().isEmpty()) {
+                        warning.setText("Please fill in all the information");
+                    } else if (res == null || res.getCount() == 0) {
+                        if (Double.parseDouble(balance.getText().toString()) < 0) {
+                            warning.setText("Please fill in a valid amount.");
+                        }
+                        if (!isValidEmail(email.getText().toString())) {    //check if provided email is valid
+                            warning.setText("Invalid email");
+                        } else {
+                            warning.setText("Success!");
+                            boolean ins = mydb.insertUser(username.getText().toString(), password.getText().toString(), Double.parseDouble(balance.getText().toString()), email.getText().toString());
+
+                            Intent intent = new Intent(getApplicationContext(), OverviewActivity.class);
+                            intent.putExtra("username", username.getText().toString());
+                            startActivity(intent);
+                        }
+                    } else {
+                        warning.setText("Username already in use!");
                     }
-                } else {
-                    warning.setText("Username already in use!");
                 }
             }
         });
